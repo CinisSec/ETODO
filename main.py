214cc3ea072debb60104c8fc261a47010172b404
grey <gregorypro@protonmail.com>
2023-04-01 01:45:25 +0200

    added addGame function

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import sqlite3
+import argparse
 
-conn = sqlite3.connect('entertainment.db')
+conn = sqlite3.connect('Entertainment.db')
 
 def createTables():
     conn.execute('''CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY,title TEXT NOT NULL)''')
     conn.execute('''CREATE TABLE IF NOT EXISTS series (id INTEGER PRIMARY KEY,title TEXT NOT NULL,season INTEGER NOT NULL, episode INTEGER NOT NULL)''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY,title TEXT NOT NULL, platform TEXT NOT NULL)''')
 
 def addMovie():
-    movieTitle = input("Please input movie title to add to DB: ")
+    movieTitle = input("Please input movie title:  ")
     conn.execute('INSERT INTO movies(title) VALUES (?)', (movieTitle,))
     conn.commit()
     print("Movie added to DB!")
 
 def addSeries():
-    seriesTitle = input("Please input movie title to add to DB: ")
-    sve = input("Did you watch a season before? [y]")
+    seriesTitle = input("Please input movie: ")
+    sve = input("Did you watch a episodes before? [y/n]")
     if sve == "y":
         snum = int(input("Which season?: [1-99] "))
         epinum = int(input("Which episode?: [1-99]"))
@@ -23,7 +25,14 @@ def addSeries():
         epinum = 1
     conn.execute('INSERT INTO series(title, season, episode) VALUES (?,?,?)', (seriesTitle,snum,epinum))
     conn.commit()
-    print("Series added to DB!")
+    print("Series added to DB!\n\n")
+
+def addGame():
+    gameTitle = input("Please input game title: ")
+    platform = input("On which platform?: ")
+    conn.execute('INSERT INTO games(title, platform) VALUES (?,?)', (gameTitle,platform))
+    conn.commit()
+    print("Game added to DB!")
 
 def getMovies():
     movies = conn.execute('SELECT * FROM movies')
